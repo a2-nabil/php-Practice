05680da28eb9ca1d366a096f33c8dd2b1f874299
a2-nabil <mdmrnbl@gmail.com>
2023-11-27 12:00:05 +0600

    String Functions

--- stringFunctions.php
+++ stringFunctions.php
@@ -19,6 +19,37 @@
     $newStr = str_replace("World", "PHP", $str);
     echo $newStr;  // Output: Hello, PHP!
 
+    $str = "Hello, World!";
+    $position = strpos($str, "World");
+    echo $position;  // Output: 7
+
+    $str = "Hello, World!";
+    $substring = substr($str, 7, 5);  // Start at position 7, take 5 characters
+    echo $substring;  // Output: World
+
+    $str = "Hello, World!";
+    $lowercase = strtolower($str);
+    $uppercase = strtoupper($str);
+    echo $lowercase;  // Output: hello, world!
+    echo $uppercase;  // Output: HELLO, WORLD!
+
+    $str = "   Hello, World!   ";
+    $trimmedStr = trim($str);
+    echo $trimmedStr;  // Output: Hello, World!
+
+    $arr = ["apple", "banana", "orange"];
+    $str1 = implode(", ", $arr);
+    echo $str1;  // Output: apple, banana, orange
+
+    $str2 = "apple, banana, orange";
+    $arr2 = explode(", ", $str2);
+    print_r($arr2);  // Output: Array ( [0] => apple [1] => banana [2] => orange )
+
+    $str = "Hello, World!";
+    $reversedStr = strrev($str);
+    echo $reversedStr;  // Output: !dlroW ,olleH
+
+
 
 
 
